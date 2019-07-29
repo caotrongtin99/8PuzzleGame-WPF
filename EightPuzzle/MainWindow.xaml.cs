32ef759e08e2c33ffb46a732912986bd501631e0
caotrongtin99 <tin.caotrong@gmail.com>
2019-07-29 19:19:38 +0700

    Handle Window Key Dowm Event

--- EightPuzzle/MainWindow.xaml.cs
+++ EightPuzzle/MainWindow.xaml.cs
@@ -49,7 +49,7 @@ namespace EightPuzzle
             var screen = new OpenFileDialog();
 
             if (screen.ShowDialog() == true)
-            { 
+            {
                 bitmap = new BitmapImage(new Uri(screen.FileName));
                 Debug.WriteLine(screen.FileName);
                 var orgImage = bitmap;
@@ -152,8 +152,8 @@ namespace EightPuzzle
         Image selectedImage = null;
         int newi = -1;
         int newj = -1;
-        int oldi = -1;
-        int oldj = -1;
+        int oldi = 2;
+        int oldj = 2;
         //int maxi = ;
         int maxj = -1;
         private void Container_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -192,11 +192,13 @@ namespace EightPuzzle
                 else
                 {
                     isDragging = false;
-                 
+
                     Canvas.SetLeft(selectedImage, oldi * (imageWidth + 5));
                     Canvas.SetTop(selectedImage, oldj * (imageHeight + 5));
-                    
+
                 }
+
+
 
             }
         }
@@ -205,7 +207,7 @@ namespace EightPuzzle
         {
             isDragging = false;
 
-            if (selectedImage != null && ((newi == oldi + 1 && newj == oldj && oldi < 2 &&  images[newi,newj]==null) || (newi == oldi - 1 && newj == oldj && oldi >= 1 && images[newi, newj] == null)
+            if (selectedImage != null && ((newi == oldi + 1 && newj == oldj && oldi < 2 && images[newi, newj] == null) || (newi == oldi - 1 && newj == oldj && oldi >= 1 && images[newi, newj] == null)
                 || (newi == oldi && newj == oldj + 1 && oldj < 2 && images[newi, newj] == null) || (newi == oldi && newj == oldj - 1 && oldj >= 1 && images[newi, newj] == null))
                 && newi < 3 && newj < 3)
             {
@@ -231,7 +233,7 @@ namespace EightPuzzle
             }
 
 
-            if (isWin() == true) 
+            if (isWin() == true)
             {
                 Timer.Stop();
                 MessageBox.Show("You Win");
@@ -273,7 +275,7 @@ namespace EightPuzzle
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            Timer.Stop();
+            //Timer.Stop();
             var screen = new SaveFileDialog();
 
             if (screen.ShowDialog() == true)
@@ -282,7 +284,7 @@ namespace EightPuzzle
                 var writer = new StreamWriter(screen.FileName);
 
                 writer.WriteLine($"IsEnded={isEnded}");
-                writer.WriteLine("Time: 00:0" + time / 60+ ":" + time % 60);
+                writer.WriteLine("Time: 00:0" + time / 60 + ":" + time % 60);
                 writer.WriteLine($"Bitmap={bitmap}");
                 for (int i = 0; i < 3; i++)
                 {
@@ -306,7 +308,7 @@ namespace EightPuzzle
                 var reader = new StreamReader(screen.FileName);
                 // 1st line
                 var firstLine = reader.ReadLine();
-                var tokens = firstLine.Split(new String[] { "=" },StringSplitOptions.RemoveEmptyEntries);
+                var tokens = firstLine.Split(new String[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
                 Debug.Write(tokens[1]);
                 //isEnded = bool.Parse(tokens[1]);
                 // 2nd Line
@@ -328,13 +330,13 @@ namespace EightPuzzle
                 // Game state
                 int[,] a = new int[3, 3];
                 var line = reader.ReadLine(); Debug.WriteLine(line);
-                tokens = line.Split(new String[] { " " },StringSplitOptions.RemoveEmptyEntries);
+                tokens = line.Split(new String[] { " " }, StringSplitOptions.RemoveEmptyEntries);
                 a[0, 0] = int.Parse(tokens[0]);
                 a[1, 0] = int.Parse(tokens[1]);
                 a[2, 0] = int.Parse(tokens[2]);
 
                 line = reader.ReadLine(); Debug.WriteLine(line);
-                tokens = line.Split(new String[] { " " },StringSplitOptions.RemoveEmptyEntries);
+                tokens = line.Split(new String[] { " " }, StringSplitOptions.RemoveEmptyEntries);
                 a[0, 1] = int.Parse(tokens[0]);
                 a[1, 1] = int.Parse(tokens[1]);
                 a[2, 1] = int.Parse(tokens[2]);
@@ -354,14 +356,14 @@ namespace EightPuzzle
 
                         var k = a[j, i];
                         Debug.WriteLine("ImgIDD:" + k);
-                        if (k==0)
+                        if (k == 0)
                         {
                             continue;
                         }
                         // Căt ảnh tại tọa độ top-left của imgID
 
                         var cropped = new CroppedBitmap(bitmap, new Int32Rect(
-                            (int)(temp_poolj[k-1] * bitmap.PixelWidth / 3), (int)(temp_pooli[k-1] * bitmap.PixelHeight / 3),
+                            (int)(temp_poolj[k - 1] * bitmap.PixelWidth / 3), (int)(temp_pooli[k - 1] * bitmap.PixelHeight / 3),
                             ((int)bitmap.PixelWidth / 3), ((int)bitmap.PixelHeight / 3)));
                         Debug.WriteLine(temp_pooli[k - 1]);
                         Debug.WriteLine(temp_poolj[k - 1]);
@@ -369,7 +371,7 @@ namespace EightPuzzle
                         var imageView = new Image();
                         imageView.Source = cropped;
                         imageView.Width = imageWidth;
-                        imageView.Height = imageHeight; 
+                        imageView.Height = imageHeight;
                         container.Children.Add(imageView);
 
                         Canvas.SetLeft(imageView, i * (imageWidth + padding));
@@ -387,6 +389,145 @@ namespace EightPuzzle
                 Debug.Write(time);
                 reader.Close();
             }
+        }
+
+        // KEY EVENT
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Down)
+            {
+
+                Debug.Write("TOA DO cua o trong:");
+                Debug.Write(oldi + " " + oldj);
+                Debug.WriteLine("");
+                if (oldi >= 0 && oldi <= 2 && oldj < 2 && oldj >= 0)
+                {
+                    Image isMoved = images[oldi, oldj + 1];
+
+                    // Toa do moi : 2 0
+                    // Buc anh bi di chuyen 2 1
+                    Canvas.SetLeft(isMoved, oldi * (imageWidth + padding));
+                    Canvas.SetTop(isMoved, oldj * (imageHeight + padding));
+
+                    SwapNum(ref imgID[oldj, oldi], ref imgID[oldj + 1, oldi]);
+                    images[oldi, oldj] = images[oldi, oldj + 1];
+                    images[oldi , oldj + 1] = null;
+                    oldj = oldj + 1;
+                }
+                else
+                {
+                    MessageBox.Show("Không thể thực hiện bước đi này!");
+                }
+
+                if (isWin())
+                {
+                    MessageBox.Show("You Win");
+                }
+                Debug.Write("Toa do moi la :" + oldi + " " + oldj);
+                Debug.WriteLine("");
+
+
+            }
+            else if (e.Key == Key.Up)
+            {
+                Debug.WriteLine("TOA DO cua o trong");
+                Debug.Write(oldi + " " + oldj);
+                Debug.WriteLine("");
+                if (oldi >= 0 && oldi <= 2 && oldj <= 2 && oldj >= 1)
+                {
+                    // Phan anh duoc di chuyen
+                    Image isMoved = images[oldi, oldj - 1];
+
+                    // Toa do moi : 2 0
+                    Canvas.SetLeft(isMoved, oldi * (imageWidth + padding));
+                    Canvas.SetTop(isMoved, oldj * (imageHeight + padding));
+
+                    SwapNum(ref imgID[oldj, oldi], ref imgID[oldj - 1, oldi]);
+                    images[oldi, oldj] = images[oldi, oldj - 1];
+                    images[oldi , oldj -1] = null;
+                    oldj = oldj - 1;
+                }
+                else
+                {
+                    MessageBox.Show("Không thể thực hiện bước đi này!");
+                }
+
+                if (isWin())
+                {
+                    MessageBox.Show("You Win");
+                }
+            }
+
+            else if (e.Key == Key.Left)
+            {
+                Debug.WriteLine("TOA DO cua o trong");
+                Debug.Write(oldi + " " + oldj);
+                Debug.WriteLine("");
+                if (oldi >= 1 && oldi <= 2 && oldj <= 2 && oldj >= 0)
+                {
+                    // Phan anh duoc di chuyen
+                    Image isMoved = images[oldi - 1, oldj];
+
+                    // Toa do moi : 2 0
+                    Canvas.SetLeft(isMoved, oldi * (imageWidth + padding));
+                    Canvas.SetTop(isMoved, oldj * (imageHeight + padding));
+
+                    SwapNum(ref imgID[oldj, oldi], ref imgID[oldj, oldi - 1]);
+                    images[oldi, oldj] = images[oldi -1, oldj];
+                    images[oldi - 1, oldj] = null;
+                    oldi = oldi - 1;
+                }
+                else
+                {
+                    MessageBox.Show("Không thể thực hiện bước đi này!");
+                }
+
+                if (isWin())
+                {
+                    MessageBox.Show("You Win");
+                }
+            }
+            else if (e.Key == Key.Right)
+            {
+                Debug.WriteLine("TOA DO cua o trong");
+                Debug.Write(oldi + " " + oldj);
+                Debug.WriteLine("");
+                if (oldi >= 0 && oldi < 2 && oldj <= 2 && oldj >= 0)
+                {
+                    // Phan anh duoc di chuyen
+                    Image isMoved = images[oldi + 1, oldj];
+
+                    // Toa do moi : 2 0
+                    Canvas.SetLeft(isMoved, oldi * (imageWidth + padding));
+                    Canvas.SetTop(isMoved, oldj * (imageHeight + padding));
+
+                    SwapNum(ref imgID[oldj, oldi], ref imgID[oldj, oldi + 1]);
+                    images[oldi, oldj] = images[oldi + 1, oldj];
+                    images[oldi + 1, oldj] = null;
+                    oldi = oldi + 1;
+                }
+                else
+                {
+                    MessageBox.Show("Không thể thực hiện bước đi này!");
+                }
+
+                if (isWin())
+                {
+                    MessageBox.Show("You Win");
+                }
+            }
+
+            Debug.Write("Toa do moi la :" + oldi + " " + oldj);
+            Debug.WriteLine("");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Debug.Write(imgID[i, j]);
+                }
+                Debug.WriteLine("");
+            }
+
         }
     }
 }
